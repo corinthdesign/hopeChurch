8096ec570b02fcec85e9190ef93035fe72c8c15e
corinthdesign <tim@corinthdesign.com>
2024-01-29 14:39:32 -0800

    whole code

--- functions/getStreams.js
+++ functions/getStreams.js
@@ -1,5 +1,14 @@
+// functions/getStreams.js
 
-// getStreams.js
+const { google } = require('googleapis');
+
+const API_KEY = 'AIzaSyCHS4L8HmcPQYwOcGFZrWAqlUrBQKt7b3E';
+const CHANNEL_ID = 'UC5dIymK_x_NSNdqE7P5FETQ';
+
+const youtube = google.youtube({
+  version: 'v3',
+  auth: API_KEY,
+});
 
 exports.handler = async function(event, context) {
   try {
@@ -27,3 +36,21 @@ exports.handler = async function(event, context) {
     };
   }
 };
+
+async function getUpcomingStreams(channelId) {
+  const response = await youtube.search.list({
+    part: 'id',
+    channelId: channelId,
+    eventType: 'upcoming',
+    type: 'video',
+  });
+
+  const upcomingStreams = response.data.items.map(item => item.id.videoId);
+  return upcomingStreams;
+}
+
+function replaceVideoId(url, newVideoId) {
+  const parsedUrl = new URL(url);
+  parsedUrl.searchParams.set('v', newVideoId);
+  return parsedUrl.toString();
+}
